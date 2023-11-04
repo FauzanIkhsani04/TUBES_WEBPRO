1c874ac83257ccf948c1afb7ba8be65de0152830
fauzanikhsani <fauzanikhsani@student.telkomuniversity.ac.id>
2023-11-04 20:54:54 +0700

    fix

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    header.addEventListener("click", function () {
+        alert("Selamat Datang di Organisasi ppppp!");
+    });
+});
